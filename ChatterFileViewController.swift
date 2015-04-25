652c86bec081cb8b9ec95ac265ed6f2f3beec366
Marti <msarigul@ucdavis.edu>
2015-04-25 15:40:01 -0700

    FFT working in real-time in ChatterFileViewController

--- ChatterFileViewController.swift
+++ ChatterFileViewController.swift
@@ -12,54 +12,44 @@ import CoreData
 
 class ChatterFileViewController : UIViewController {
     
-    let microphone: Microphone
-    
-     init(){
-        microphone = Microphone()
-        super.init()
-    }
-    
-    required init(coder aDecoder: NSCoder) {
-//        var baseString : String = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0] as! String
-//        self.audioURL = NSUUID().UUIDString + ".m4a"
-//        var pathComponents = [baseString, self.audioURL]
-//        var audioNSURL = NSURL.fileURLWithPathComponents(pathComponents)
-//        
-//        var session = AVAudioSession.sharedInstance()
-//        session.setCategory(AVAudioSessionCategoryPlayAndRecord, error: nil)
-//        
-//        var recordSettings: [NSObject : AnyObject] = Dictionary()
-//        recordSettings[AVFormatIDKey] = kAudioFormatMPEG4AAC
-//        recordSettings[AVSampleRateKey] = 44100.0
-//        recordSettings[AVNumberOfChannelsKey] = 2
-//        
-//        self.audioRecorder = AVAudioRecorder(URL: audioNSURL, settings: recordSettings, error: nil)
-//        self.audioRecorder.meteringEnabled = true
-//        self.audioRecorder.prepareToRecord()
-        
-        microphone = Microphone()
-        
-        // super init below
-        super.init(coder: aDecoder)
-    }
-    
+
+    @IBOutlet var fftPlot: AKAudioInputFFTPlot!
+    let microphone = Microphone()
     
     @IBOutlet weak var chatterTextField: UITextField! // chatter file name text field
     @IBOutlet weak var recordButton: UIButton!
-    
-  
     
 //    var audioRecorder : AVAudioRecorder
 //    var audioURL: String
     var previousViewController = MainTableViewController()
     
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        AKManager.sharedManager().isLogging = true
+        AKOrchestra.addInstrument(microphone)
         // LGI
         microphone.play()
+        AKManager.addBinding(fftPlot)
+
+//        var baseString : String = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0] as! String
+//        self.audioURL = NSUUID().UUIDString + ".m4a"
+//        var pathComponents = [baseString, self.audioURL]
+//        var audioNSURL = NSURL.fileURLWithPathComponents(pathComponents)
+//
+//        var session = AVAudioSession.sharedInstance()
+//        session.setCategory(AVAudioSessionCategoryPlayAndRecord, error: nil)
+//
+//        var recordSettings: [NSObject : AnyObject] = Dictionary()
+//        recordSettings[AVFormatIDKey] = kAudioFormatMPEG4AAC
+//        recordSettings[AVSampleRateKey] = 44100.0
+//        recordSettings[AVNumberOfChannelsKey] = 2
+//
+//        self.audioRecorder = AVAudioRecorder(URL: audioNSURL, settings: recordSettings, error: nil)
+//        self.audioRecorder.meteringEnabled = true
+//        self.audioRecorder.prepareToRecord()
     }
+    
     // cancel button
     @IBAction func cancelTapped(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
